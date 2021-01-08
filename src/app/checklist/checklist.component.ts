6c62ad2e9e889da4cb209a4b7a06ed9038b0f39a
dx-pccw <da-xuan.chew@pccw.com>
2021-01-08 16:29:50 +0800

    3 files modified

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface Task {
+  name: string;
+  completed: boolean;
+  subtasks?: Task[];
+}
+
 @Component({
   selector: 'app-checklist',
   templateUrl: './checklist.component.html',
   styleUrls: ['./checklist.component.css']
 })
-export class ChecklistComponent implements OnInit {
+export class ChecklistComponent{
+  task: Task = {
+    name: 'Indeterminate',
+    completed: false,
+    subtasks: [
+      {name: 'Primary', completed: false},
+      {name: 'Accent', completed: false},
+      {name: 'Warn', completed: false}
+    ]
+  };
 
-  constructor() { }
+  allComplete: boolean = false;
 
-  ngOnInit() {
+  updateAllComplete() {
+    this.allComplete = this.task.subtasks != null && this.task.subtasks.every(t => t.completed);
+  }
+
+  someComplete(): boolean {
+    if (this.task.subtasks == null) {
+      return false;
+    }
+    return this.task.subtasks.filter(t => t.completed).length > 0 && !this.allComplete;
+  }
+
+  setAll(completed: boolean) {
+    this.allComplete = completed;
+    if (this.task.subtasks == null) {
+      return;
+    }
+    this.task.subtasks.forEach(t => t.completed = completed);
   }
 
 }
