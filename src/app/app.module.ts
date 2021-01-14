e23a89b37afb226cc9c71811a9f897ca23072ee9
dx-pccw <da-xuan.chew@pccw.com>
2021-01-14 10:11:11 +0800

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ChecklistComponent } from './checklist/checklist.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ChecklistComponent ],
+  declarations: [ AppComponent, HelloComponent, ChecklistComponent, TopBarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
